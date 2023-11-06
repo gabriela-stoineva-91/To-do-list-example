0ad979c599dc56e1bf168ff23786c78b6f7d99a6
gabriela-stoineva-91 <114081727+gabriela-stoineva-91@users.noreply.github.com>
2023-11-06 12:38:16 +0200

    add loader styles

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,8 +1,10 @@
+import styles from './Loader.module.css'
+
 export default function Loader() {
     return (
-        <div className="loading-container">
-          <div className="loading-spinner">
-            <span className="loading-spinner-text">Loading</span>
+        <div className={styles["loading-container"]}>
+          <div className={styles["loading-spinner"]}>
+            <span className={styles["loading-spinner-text"]}>Loading</span>
           </div>
         </div>
     )
