fdb74888b32d5d2685912bf793d27c9ba156b872
gabriela-stoineva-91 <114081727+gabriela-stoineva-91@users.noreply.github.com>
2023-11-06 13:16:02 +0200

    Add table component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
-import './App.css'
-import Header from './components/Header'
-import Loader from './components/Loader'
+import { useState } from 'react';
+import './App.css';
+import Header from './components/Header';
+import Loader from './components/Loader';
+import Footer from './components/Footer';
+import TableContainer from './components/TableContainer';
 
 function App() {
 
   return (
     <div className="root">
       <Header />
-      <Loader/>
+      <Loader />
+      <Footer />
+      <TableContainer />
     </div>
   )
 }
